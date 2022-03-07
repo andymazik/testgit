f3a506237606335d9f2c14236398a8c9933e0171
andymazik <89570841+andymazik@users.noreply.github.com>
2022-03-07 20:39:17 +0300

    Update Class1.java

--- src/Class1.java
+++ src/Class1.java
@@ -2,6 +2,8 @@ public class Class1 {
     public static void main(String[] args) {
         System.out.println("Text for 2 commit");
         System.out.println("Text for 2 commit");
+        System.out.println("Text for 2 commit");
+        System.out.println("Text for 2 commit");
 
     }
 }
