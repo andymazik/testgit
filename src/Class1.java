392b70b38d45a3bd35da3ba0b1e14dd3156bf269
Andrey Mazikov <andymazik@gmail.com>
2022-03-07 19:50:33 +0300

    4th. Added another text

--- src/Class1.java
+++ src/Class1.java
@@ -1,8 +1,8 @@
 public class Class1 {
     public static void main(String[] args) {
         System.out.println("Text1");
-        System.out.println("Text1");
 
+        System.out.println("Text for 2 commit");
         System.out.println("Text for 2 commit");
 
     }
--- src/Class2.java
+++ src/Class2.java
@@ -1,8 +1,8 @@
 public class Class2 {
     public static void main(String[] args) {
         System.out.println("Text2");
-        System.out.println("Text2");
 
+        System.out.println("Text for 2 commit");
         System.out.println("Text for 2 commit");
     }
 }
