9d9f3720b38186f5a7c60cdbbab13e4172403d1a
Andrey Mazikov <andymazik@gmail.com>
2022-03-07 20:15:33 +0300

    5th. Deleted text

--- src/Class2.java
+++ src/Class2.java
@@ -1,6 +1,6 @@
 public class Class2 {
     public static void main(String[] args) {
-        System.out.println("Text2");
+        System.out.println("newText");
 
         System.out.println("Text for 2 commit");
         System.out.println("Text for 2 commit");
