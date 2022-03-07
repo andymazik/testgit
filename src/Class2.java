fc982a3bbdcc91f88acd8636ded7c3126d0fdddb
Andrey Mazikov <andymazik@gmail.com>
2022-03-07 19:41:49 +0300

    2nd. Add new text

--- src/Class2.java
+++ src/Class2.java
@@ -2,5 +2,7 @@ public class Class2 {
     public static void main(String[] args) {
         System.out.println("Text2");
         System.out.println("Text2");
+
+        System.out.println("Text for 2 commit");
     }
 }
